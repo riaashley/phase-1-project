f1d7d176840034897d236accdb33b34256748d88
Ria Legaspi <riaveluz@gmail.com>
2022-09-13 22:27:55 -0700

    collected data from api

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,15 @@ const listedShows = document.getElementById('listed-shows')
 
 showsDropdown.addEventListener('change', alphabetical)
 
+allShows()
+
+
+//Collect list of Show Names
+function allShows(){
+    fetch('https://api.tvmaze.com/shows')
+    .then(resp => resp.json())
+    .then(data => console.log('data', data))
+}
 
 //Grab value in dropdown
 function alphabetical(e){
