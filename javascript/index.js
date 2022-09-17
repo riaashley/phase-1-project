e817ba0e2aab05a396654d10e6c7ba50627865d3
Ria Legaspi <riaveluz@gmail.com>
2022-09-16 18:32:24 -0700

    functions added for html tags

--- javascript/index.js
+++ javascript/index.js
@@ -21,33 +21,41 @@ function allShows(){
 //Grab value in dropdown and attach details
 function alphabetical(e){
     const letter = e.target.value
+    listedShows.innerHTML = ""
     const filteredShows = show.filter(individualShow => individualShow.name.startsWith(letter))
+    eachShow(filteredShows)
+}
+    
+function eachShow(filteredShows){
     filteredShows.forEach(show => {
-        console.log(show)
-        const li = document.createElement('li')
-        li.innerHTML = show.name
-        listedShows.append(li)
-        const img = document.createElement('img')
-        img.src = show.image.original
-        img.style.height = '7rem'
-        const p = document.createElement('p')
-        p.innerHTML = show.summary
-        li.append(img, p)
-        listedShows.append(li)
-        })
-    }
+    const li = document.createElement('li')
+    li.innerHTML = show.name
+    listedShows.append(li)
+    const img = document.createElement('img')
+    img.src = show.image.original
+    img.style.height = '7rem'
+    const p = document.createElement('p')
+    p.innerHTML = show.summary
+    li.append(img, p)
+    listedShows.append(li)
+    })
+}
 
     //Grab value for submit button and attach random show
-  function randomShow(){
+function randomShow(){
+    randomListed.innerHTML = ''
     const newShow = show[Math.floor(Math.random() * 240)]
-    console.log(newShow)
-        let li = document.createElement('li')
-        li.innerHTML = newShow.name
-        let img = document.createElement('img')
-        img.src = newShow.image.original
-        img.style.height = '8rem'
-        let p = document.createElement('p')
-        p.innerHTML = newShow.summary
-        li.append(img, p)
-        randomListed.append(li)
+    addInformation(newShow)
+  }
+
+function addInformation(newShow){
+    let li = document.createElement('li')
+    li.innerHTML = newShow.name
+    let img = document.createElement('img')
+    img.src = newShow.image.original
+    img.style.height = '8rem'
+    let p = document.createElement('p')
+    p.innerHTML = newShow.summary
+    li.append(img, p)
+    randomListed.append(li)
     }
